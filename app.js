fb7bd9bc3e20165aa2e10bc83d0e0e6d31f65be6
권우민 <34460348+yvsc5020@users.noreply.github.com>
2022-12-06 19:32:01 +0900

    name 수정 과제

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const commonRouter = require('./routes/common');
-app.use('/common', commonRouter);
+const commonRouter = require("./routes/common");
+app.use("/common", commonRouter);
 
-const userRouter = require('./routes/user');
-app.use('/user', userRouter);
+const userRouter = require("./routes/user");
+app.use("/user", userRouter);
 
 const socketPort = process.env.SOCKET_PORT || 3001;
-const { Server } = require('socket.io');
-const { OTHER, TOKEN_OR_REFRESH_EMPTY, TOKEN_EXPIRED, TOKEN_INVALID } = require('./modules/ERROR');
-const { checkTokenSocket } = require('./middlewares/auth');
-const jwt = require('jsonwebtoken');
+const { Server } = require("socket.io");
+const {
+  OTHER,
+  TOKEN_OR_REFRESH_EMPTY,
+  TOKEN_EXPIRED,
+  TOKEN_INVALID,
+} = require("./modules/ERROR");
+const { checkTokenSocket } = require("./middlewares/auth");
+const jwt = require("jsonwebtoken");
 const io = new Server(socketPort);
 
 io.use(async (socket, next) => {
   try {
-    console.log('test1');
     await checkTokenSocket(socket.request);
 
     next();
@@ -44,22 +48,21 @@ io.use(async (socket, next) => {
 
 io.use((socket, next) => {
   try {
-    console.log('test2');
-    const token = socket.request.headers.authorization.replace('Bearer ', '');
+    const token = socket.request.headers.authorization.replace("Bearer ", "");
 
     socket.userId = jwt.decode(token).id;
     next();
   } catch (error) {
     next(error);
   }
-})
+});
 
-io.on('connection', socket => {
+io.on("connection", (socket) => {
   console.log(`connection! ${socket.userId}`);
 
-  socket.on('message', message => {
+  socket.on("message", (message) => {
     console.log(message);
-    socket.emit('test', 'test');
+    socket.emit("test", "test");
   });
 });
 
--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,20 @@
-const router = require('express').Router();
-const util = require('../modules/util');
-const User = require('../schema/User');
+const router = require("express").Router();
+const util = require("../modules/util");
+const User = require("../schema/User");
 
-const authUtil = require('../middlewares/auth').checkToken;
+const authUtil = require("../middlewares/auth").checkToken;
 
-const encodePassword = require('../modules/crypto');
-const { sign, refresh, isExpired } = require('../middlewares/jwt');
-const { 
-  EMPTY_INFO: { USER_ID, USER_PASSWORD, USERNAME }, 
-  WRONG_PASSWORD: { IN_KOREAN }, 
-  OTHER, 
-  EXISTS_ID, 
-  NOT_EXISTS_USER 
-} = require('../modules/ERROR');
+const encodePassword = require("../modules/crypto");
+const { sign, refresh, isExpired } = require("../middlewares/jwt");
+const {
+  EMPTY_INFO: { USER_ID, USER_PASSWORD, USERNAME },
+  WRONG_PASSWORD: { IN_KOREAN },
+  OTHER,
+  EXISTS_ID,
+  NOT_EXISTS_USER,
+} = require("../modules/ERROR");
 
-router.post('/signUp', async (req, res) => {
+router.post("/signUp", async (req, res) => {
   // 1. 필요한 변수 값을 받는다.
   const { id, password, name } = req.body;
 
@@ -37,13 +37,15 @@ router.post('/signUp', async (req, res) => {
 
     // 5. 데이터를 DB에 저장한다.
     await User.create({
-      id, password: encodedPassword, name
+      id,
+      password: encodedPassword,
+      name,
     });
 
     // 6. client에게 message를 던져준다.
-    return res.json(util.success('SUCCESS_SIGN_UP', 'Success sign up!'));
+    return res.json(util.success("SUCCESS_SIGN_UP", "Success sign up!"));
   } catch ({ code, message }) {
-    switch(code) {
+    switch (code) {
       case USER_ID.code:
         return res.json(util.fail(USER_ID.code, USER_ID.message));
       case USER_PASSWORD.code:
@@ -61,7 +63,7 @@ router.post('/signUp', async (req, res) => {
   }
 });
 
-router.post('/signIn', async (req, res) => {
+router.post("/signIn", async (req, res) => {
   const { id, password } = req.body;
 
   try {
@@ -86,22 +88,27 @@ router.post('/signIn', async (req, res) => {
     let { refreshToken } = user;
     if (isExpired(refreshToken)) {
       refreshToken = await refresh();
-      await User.updateOne({ id: user.id }, {$set: { refreshToken }});
+      await User.updateOne({ id: user.id }, { $set: { refreshToken } });
     }
 
     const options = {
-      token, refreshToken
+      token,
+      refreshToken,
     };
     // 5. client에게 message를 던져준다.
-    return res.json(util.success('SUCCESS_SIGN_IN', 'Success sign in!', options))
+    return res.json(
+      util.success("SUCCESS_SIGN_IN", "Success sign in!", options)
+    );
   } catch ({ code, message }) {
-    switch(code) {
+    switch (code) {
       case USER_ID.code:
         return res.json(util.fail(USER_ID.code, USER_ID.message));
       case USER_PASSWORD.code:
         return res.json(util.fail(USER_PASSWORD.code, USER_PASSWORD.message));
       case NOT_EXISTS_USER.code:
-        return res.json(util.fail(NOT_EXISTS_USER.code, NOT_EXISTS_USER.message));
+        return res.json(
+          util.fail(NOT_EXISTS_USER.code, NOT_EXISTS_USER.message)
+        );
       case IN_KOREAN.code:
         return res.json(util.fail(IN_KOREAN.code, IN_KOREAN.message));
       case EXISTS_ID.code:
@@ -114,8 +121,45 @@ router.post('/signIn', async (req, res) => {
 });
 
 // 1. 임의 User의 name을 바꾸는 API를 구현하시면 됩니다.
-router.post('/changeUsername', authUtil, async (req, res) => {
-  return res.json(util.success('test', 'test', 'test'));
+router.post("/changeUsername", authUtil, async (req, res) => {
+  const { id, changeName } = req.body;
+
+  try {
+    if (!id) {
+      throw USER_ID;
+    } else if (!changeName) {
+      throw CHANGENAME;
+    }
+
+    const user = await User.findOne({ id });
+    if (!user) {
+      throw NOT_EXISTS_USER;
+    }
+
+    await User.updateOne({ id: user.id }, { $set: { name: changeName } });
+
+    return res.json(
+      util.success("SUCCESS_CHANGE_NAME", "Success change name!")
+    );
+  } catch ({ code, message }) {
+    switch (code) {
+      case USER_ID.code:
+        return res.json(util.fail(USER_ID.code, USER_ID.message));
+      case USER_PASSWORD.code:
+        return res.json(util.fail(USER_PASSWORD.code, USER_PASSWORD.message));
+      case NOT_EXISTS_USER.code:
+        return res.json(
+          util.fail(NOT_EXISTS_USER.code, NOT_EXISTS_USER.message)
+        );
+      case IN_KOREAN.code:
+        return res.json(util.fail(IN_KOREAN.code, IN_KOREAN.message));
+      case EXISTS_ID.code:
+        return res.json(util.fail(EXISTS_ID.code, EXISTS_ID.message));
+      default:
+        console.log(message);
+        return res.json(util.fail(OTHER.code, OTHER.message));
+    }
+  }
 });
 
 module.exports = router;
